227324b9bf315344e2af78aba05035f4b276d8ad
Pedro Goncalves <pedrogcost@gmail.com>
2025-03-06 13:31:43 -0300

    adiciona funcoes em Alugames

--- alugames/js/app.js
+++ alugames/js/app.js
@@ -1,17 +1,33 @@
+// exibi a quantidade de jogos que já foram alugados
+function exibirQuantidadeLocacoes() {
+    let palavraJogo = numeroLocacoes > 1 ? 'jogos': 'jogo';
+    console.log(`Já foram alugados ${numeroLocacoes} ${palavraJogo}`);
+}
+
 function alterarStatus(id) {
-    let jogoCliclado = document.getElementById(`game-${id}`);
-    let imagem = jogoCliclado.querySelector('.dashboard__item__img');
-    let botao = jogoCliclado.querySelector('.dashboard__item__button');
+    // capturar elementos da tela
+    let jogoClicado = document.getElementById(`game-${id}`);
+    let imagem = jogoClicado.querySelector('.dashboard__item__img');
+    let botao = jogoClicado.querySelector('.dashboard__item__button');
+    let nomeJogo = jogoClicado.querySelector('.dashboard__item__name');
 
+    // mudar aparencia e texto dos jogos ao serem alugados/devolvivdos
     if (imagem.classList.contains('dashboard__item__img--rented')) {
-        imagem.classList.remove('dashboard__item__img--rented');
-        botao.classList.remove('dashboard__item__button--return');
-        botao.textContent = 'Alugar';
-
+        // adiciona confirmação pro usuário devolver o game
+        if(confirm(`Tem certeza que você deseja devolver o jogo ${nomeJogo.textContent}`)) {
+            imagem.classList.remove('dashboard__item__img--rented');
+            botao.classList.remove('dashboard__item__button--return');
+            botao.textContent = 'Alugar';   
+        }
     } else {
         imagem.classList.add('dashboard__item__img--rented');
         botao.classList.add('dashboard__item__button--return');
         botao.textContent = 'Devolver';
-
+        numeroLocacoes++;
+        }
+        exibirQuantidadeLocacoes();
     }
-}
+    
+
+let numeroLocacoes = 0;
+
